7079228a39c458c6de12f445b112d62395ded4e1
Michael Szerman <michaelszer@gmail.com>
2019-11-11 08:47:17 -0300

    Final commit

---
+++ build/precache-manifest.703ac4f8112c343e6e36a65a124e3833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b08bf03981d72694701f02070caa27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "671acf88ebdb192354fa",
+    "url": "/static/css/main.97808560.chunk.css"
+  },
+  {
+    "revision": "9595126d89d83dc799e5",
+    "url": "/static/js/2.963120d6.chunk.js"
+  },
+  {
+    "revision": "671acf88ebdb192354fa",
+    "url": "/static/js/main.adc7059c.chunk.js"
+  },
+  {
+    "revision": "2130526a43f1de831b97",
+    "url": "/static/js/runtime-main.b6f7a664.js"
+  },
+  {
+    "revision": "8eb84c216dce5d3d78ee8c746f35af38",
+    "url": "/static/media/aaveLogo.8eb84c21.svg"
+  }
+]);
